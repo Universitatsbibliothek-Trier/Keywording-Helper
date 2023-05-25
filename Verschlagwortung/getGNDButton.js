f5fba17ebb6f542629b77b88588e40c2071e230f
René Ackels <ackels@uni-trier.de>
2023-05-25 17:05:40 +0200

    lots of stuff like list

---
+++ Verschlagwortung/getGNDButton.js
@@ -0,0 +1,126 @@
+'use strict'
+
+let header = "GND";
+let header_text =`${header} Verschlagwortungsgehilfe`;
+document.getElementById('headerText').innerHTML = header_text;
+
+// mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button--raised'));
+// const list = new MDCList(document.querySelector('.mdc-list'));
+// const listItemRipples = list.listElements.map((listItemEl) => new MDCRipple(listItemEl));
+
+async function getNewGND()
+{
+  await getGND();
+  // await document.getElementById('aktualisierungsButton').click;
+  setTimeout(aktualisiereListe,1000);
+  // wait(7000);
+  // aktualisiereListe();
+}
+async function getGND()
+{
+  
+  var gndNumber = document.querySelector('.mdc-text-field').MDCTextField.value;
+  
+  let partURLGnd = "https://lobid.org/gnd/";  
+  gndNumber = gndNumber.replace(/ /g,'');
+  let URLGnd = partURLGnd + gndNumber;
+  const responseGND = await fetch(URLGnd);  
+
+  
+  let partURL = "https://0.0.0.0:9200/gnd/_doc/";
+  var jsonGND = await responseGND.json();
+  console.log(jsonGND.gndIdentifier);
+  jsonGND.vorkommen = 1;
+  var gndIdentifier = jsonGND.gndIdentifier;
+
+  const response = await fetch(partURL, 
+     {
+      mode: 'cors',
+      credentials: "include",
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body:  JSON.stringify( {
+        jsonGND
+        //todo
+      // "anzahl":  0
+     })
+     }
+  ); 
+     
+  
+  
+  // partURL = ("https://0.0.0.0:9200/gnd/_refresh");
+  // const responseRefresh = await fetch(partURL, 
+  //   {
+  //    mode: 'cors',
+  //    credentials: "include",
+  //    method: 'POST'
+  //   //  headers: { 'Content-Type': 'application/json' }
+  //   })
+  //    ; 
+}
+    // Trier GND: 4060877-3
+    // Amphitheater: 1137516623
+    // Porta Nigra: 4474732-9
+
+
+// async function putFromElastic()
+// {
+
+//   // document.querySelector('[aria-label="gndNumber"]').MDCTextField.disabled = false;
+//   // var gndNumber = document.querySelector('.mdc-text-field').MDCTextField.value;
+  
+//   // let partURL = "https://0.0.0.0:9200/customer/_doc/1?pretty";  
+//   let partURL = "https://0.0.0.0:9200/customer/_doc/3?pretty";  
+//   // let partURL = "https://es01.bib55.uni-trier.de/_search?pretty";  
+//   // let partURL = "http://lobid.org/gnd/4074335-4";  
+//   // let partURL = "https://0.0.0.0:9200";  
+//   // let URL = partURL + gndNumber;
+//   // partURL = partURL.replace(/ /g,'');
+  
+//   // const httpsAgent = new https.Agent({
+//   //   rejectUnauthorized: false,
+//   // });
+// //   const requestOptions = {
+// //     method: 'PUT',
+// //     headers: { 'Content-Type': 'application/json' },
+// //     body: JSON.stringify( {
+// //       "firstname":  "Peter"
+// //     })
+// // };
+
+//   const response = await fetch(partURL, 
+//      {
+//       mode: 'cors',
+//       // agent: httpsAgent,
+//     // method: "GET", 
+//     // auth: { username: 'ackels', password: 'blkmgr' },// *GET, POST, PUT, DELETE, etc.
+//     credentials: "include",
+//     method: 'PUT',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify( {
+//       "firstname":  "Peter"
+//     }) // include, *same-origin, omit
+//     // headers: {
+//     //   "Content-Type": "application/json",
+//     //   // 'Content-Type': 'application/x-www-form-urlencoded',
+//     // },
+//     // body: JSON.stringify(data), // body data type must match "Content-Type" header
+//    }
+//   ); 
+
+//   console.log(response);
+//   const commits = await response.json();  
+//   // document.querySelector('.mdc-text-field').MDCTextField.value = "";
+
+
+//   // document.getElementById('preferredName').innerHTML = commits._source.firstname;
+//   // document.getElementById('variantName').innerHTML = commits._source.lastname;
+
+
+//   // const textField = new MDCTextField(document.querySelector('[aria-label="gndNumber"]'));
+//   // console.log(gndNumber);
+
+//     // Trier GND: 4060877-3
+// }
+
--- Verschlagwortung/itemList.js
+++ Verschlagwortung/itemList.js
@@ -1,45 +1,90 @@
 'use strict'
 
-var list = '<ul class="mdc-list"> \
-<li class="mdc-list-item" tabindex="0">\
-  <span class="mdc-list-item__ripple"></span>\
-  <span class="mdc-list-item__text">Item 1 - Division 1</span>\
-</li>\
-<li class="mdc-list-item">\
-  <span class="mdc-list-item__ripple"></span>\
-  <span class="mdc-list-item__text">Item 2 - Division 1</span>\
-</li>\
-<li role="separator" class="mdc-list-divider"></li>\
-<li class="mdc-list-item">\
-  <span class="mdc-list-item__ripple"></span>\
-  <span class="mdc-list-item__text">Item 1 - Division 2</span>\
-</li>\
-<li class="mdc-list-item">\
-  <span class="mdc-list-item__ripple"></span>\
-  <span class="mdc-list-item__text">Item 2 - Division 2</span>\
-</li>\
-';
+document.createElement("ul");
 
-//code to 
-let partURL = "https://0.0.0.0:9200/gnd/_doc/";
-  var jsonGND = await responseGND.json();
-  console.log(jsonGND.gndIdentifier);
+async function aktualisiereListe()
+{
+  let nodeList = document.getElementById("divList");
 
-  var gndIdentifier = jsonGND.gndIdentifier;
+  if (nodeList.hasChildNodes())
+  {
+    nodeList.removeChild(nodeList.children[0]);
+  }
+  //code to 
 
-  const response = await fetch(partURL, 
-     {
+  const listBeginning = document.createElement("ul");
+  nodeList.insertAdjacentElement("afterbegin", listBeginning);
+  listBeginning.classList.add('mdc-list');
+
+  let partURL = "https://0.0.0.0:9200/gnd/_search?size=10000";
+
+  // const responseGND = await fetch(partURL);
+
+
+  // var gndIdentifier = jsonGND.gndIdentifier;
+  // let username = 'ackels';
+  // let password = 'blkmgr';
+  // let headers = new Headers();
+  // headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
+  // headers.append('Content-Type', 'application/json');
+  const response = await fetch(partURL,
+    {
       mode: 'cors',
       credentials: "include",
-      method: 'POST',
+      method: 'GET',
+      // auth: { username: 'ackels', password: 'blkmgr' },
       headers: { 'Content-Type': 'application/json' },
-      body:  JSON.stringify( {
-        jsonGND
-        // "gndid" : gndIdentifier
-      // "anzahl":  0
-     })
-     }
-  ); 
-  
-list = list + "</ul>";
-document.write(list);
+      // body: JSON.stringify({
+
+      //   "query": {
+      //     "match_all": {}
+      //   }
+
+      // })
+    }
+  );
+
+  var jsonGND = await response.json();
+  // console.log(jsonGND);
+  console.log(jsonGND.hits.hits);
+
+  var test = "";
+  for (let x in jsonGND.hits.hits)
+  {
+    // sortierfunktion nach Anzahl einbauen
+    
+
+    const listItem = document.createElement("li");
+    listBeginning.insertAdjacentElement("afterbegin", listItem);
+    listItem.classList.add('mdc-list-item');
+    const span1 = document.createElement("span");
+    const span2 = document.createElement("span");
+    span1.classList.add('mdc-list-item__ripple');
+    span2.classList.add('mdc-list-item__text');
+    listItem.appendChild(span1);
+    listItem.appendChild(span2);
+    span2.id = "listElement1";
+    span2.innerHTML = "GND-ID:  " + jsonGND.hits.hits[x]._source.jsonGND.gndIdentifier + "   " + jsonGND.hits.hits[x]._source.jsonGND.preferredName + "   Vorkommen: 1";
+  }
+  //todo
+  //   var list = '<ul class="mdc-list"> \
+  // <li class="mdc-list-item" tabindex="0">\
+  //   <span class="mdc-list-item__ripple"></span>\
+  //   <span class="mdc-list-item__text">Item 1 - Division 1</span>\
+  // </li>\
+  // <li class="mdc-list-item">\
+  //   <span class="mdc-list-item__ripple"></span>\
+  //   <span class="mdc-list-item__text">Item 2 - Division 1</span>\
+  // </li>\
+  // <li role="separator" class="mdc-list-divider"></li>\
+  // <li class="mdc-list-item">\
+  //   <span class="mdc-list-item__ripple"></span>\
+  //   <span class="mdc-list-item__text">Item 1 - Division 2</span>\
+  // </li>\
+  // <li class="mdc-list-item">\
+  //   <span class="mdc-list-item__ripple"></span>\
+  //   <span class="mdc-list-item__text">Item 2 - Division 2</span>\
+  // </li>\
+  // ';
+
+}
