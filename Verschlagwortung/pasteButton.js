fdea97718ca1a6a532219327340f61ee7bf2f382
René Ackels <ackels@uni-trier.de>
2023-06-20 13:40:42 +0200

    paste button

--- Verschlagwortung/pasteButton.js
+++ Verschlagwortung/pasteButton.js
@@ -11,6 +11,22 @@ async function pasteFromClipboard()
 {
   // document.execCommand('paste');
   document.getElementById("rueckMeldung").innerHTML = "GND-URI eingefügt.";
-  document.getElementById("rueckMeldung").innerHTML = await navigator.clipboard.readText();
-  
+  // document.getElementById("rueckMeldung").innerHTML = await navigator.clipboard.readText();
+  // gndNumber.focus();
+  // document.execCommand("Paste", null, null);
+  // navigator.clipboard
+  //       .readText()
+  //       .then((clipText) => {
+  //             document.getElementById("gndNumber").innerHTML = clipText;
+  //           }
+  //         );
+  // document.getElementById("gndURI").innerHTML = "so ein schwachsinn";
+   navigator.clipboard
+          .readText()
+          .then((clipText) => {
+            document.getElementById("gndNumber").value = clipText;
+            // document.getElementById("rueckMeldung").innerHTML = clipText;
+            console.log("Text ist: " + clipText);
+              }
+            );
 }
