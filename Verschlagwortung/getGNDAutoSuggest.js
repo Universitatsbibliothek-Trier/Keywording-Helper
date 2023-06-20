bfccde0ba7e9a364d4a2df537483c0911cffb4d6
René Ackels <ackels@uni-trier.de>
2023-06-20 16:28:11 +0200

    offizielle GND-Suche

---
+++ Verschlagwortung/getGNDAutoSuggest.js
@@ -0,0 +1,13 @@
+
+
+async function getOfficialGND()
+{
+  let partURLGnd = "https://lobid.org/gnd/search?q=";
+
+  let gndEingabe = document.getElementById('officialGND').value;
+  let URLGnd = partURLGnd + gndEingabe + "&format=json:preferredName";
+  const responseGNDLobid = await fetch(URLGnd);
+  jsonGND = await responseGNDLobid.json();
+
+  createOfficialGNDList(jsonGND);
+}
